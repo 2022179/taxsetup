02fc39474fe0a60094a97e334afdbb7643770e78
2022179 <146199008+2022179@users.noreply.github.com>
2023-12-06 17:16:56 +0000

    datasetup
    have setup the database using boolean method

--- src/taxsetup/datasetup.java
+++ src/taxsetup/datasetup.java
@@ -4,10 +4,22 @@
  */
 package taxsetup;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Primm
  */
 public class datasetup {
+    final String DB_BASE_URL = "jdbc:mysql://localhost";
+    final String USER = "CCT";
+    final String PASSWORD ="dublin"; 
     
+    //method to set up our database
+    public boolean setupDB() throws SQLException{
+       Connection conn= DriverManager.getConnection( DB_BASE_URL,USER,PASSWORD);
+    
+}
 }
