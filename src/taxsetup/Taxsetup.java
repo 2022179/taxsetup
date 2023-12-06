f25ba83e00edb331f1257c0ae3e94d2abafd4b50
2022179 <146199008+2022179@users.noreply.github.com>
2023-12-06 13:46:01 +0000

    created two classes
    got constructors, setters and getters for the class employee.

--- src/taxsetup/Taxsetup.java
+++ src/taxsetup/Taxsetup.java
@@ -14,7 +14,11 @@ public class Taxsetup {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+       
         // TODO code application logic here
-    }
+         employee e2= new employee("John","2023-03-26","");
+         
     
+}
 }
---
+++ src/taxsetup/employee.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taxsetup;
+
+/**
+ *
+ * @author Primm
+ */
+public class employee {
+
+    //getter for tax ID
+    public static int getTaxID() {
+        return taxID;
+    }
+
+    public static void setTaxID(int aTaxID) {
+        taxID = aTaxID;
+    }
+ 
+    private String name;
+    private String birthdate;
+    private String email;
+    private int staffID;
+    private static int taxID =1;// this means its final and cant be changed 
+   //constructors 
+    public employee(String name, String birthdate, String email) {
+        this.name = name;
+        this.birthdate = birthdate;
+        this.email = email;
+        this.staffID = staffID;
+    }
+//setters 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setStaffID(int staffID) {
+        this.staffID = staffID;
+    }
+    
+}
