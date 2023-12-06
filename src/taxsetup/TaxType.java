58bae3b814ab215066693d51924790458a2c195d
2022503 <146199452+2022503@users.noreply.github.com>
2023-12-06 19:01:24 +0000

    Added a class enums TaxType
    added a class enums TaxType

---
+++ src/taxsetup/TaxType.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package taxsetup;
+
+/**
+ *
+ * @author User
+ */
+public enum TaxType {
+    INCOME_TAX,
+    USC,
+    PRSI,
+    
+}
